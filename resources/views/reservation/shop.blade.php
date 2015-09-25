e3bae278e1a062947ee7a5a8e6ba093cdc2a979a
snowix3 <snowix3@gmail.com>
2015-09-25 17:43:37 +0900

    adminshop予約機能の予約在庫数管理機能完成

--- resources/views/reservation/shop.blade.php
+++ resources/views/reservation/shop.blade.php
@@ -45,9 +45,6 @@
                 <a href="shop"><button value="shop">リロード</button></a><br>
                 <?php
                 if (isset($timeArr3)){
-                    //foreach ($timeArr3 as $key => $value) {
-                    //    echo $key.":".$value."<br>";
-                    //}
                     $i=0;
                     $j=0;
                     $hour=0;
@@ -64,11 +61,26 @@
                       //echo "<br>";
                     }
                     $jcount=0;
-                    for ($j=0; $j < 336 ; $j++) {
-                      $time[$jcount]=$j*$jcount;
-                      echo ":".$time[$jcount];
-                      echo "<br>";
-                      $jcount++;
+                    $kcount=0;
+                    $count=0;
+                    for ($j=0; $j < 48 ; $j++) {
+                      for ($k=0; $k < 7 ; $k++) {
+                        /*if ($count==0) {
+                          $time[$count]=($jcount)*48+$kcount;
+                          echo "000:".$time[$count];
+                          echo "<br>";
+                          //$jcount++;
+                          $count++;
+                        }else {*/
+                          $time[$count]=($jcount)*48+$kcount;
+                          //echo $time[$count];
+                          //echo "<br>";
+                          $count++;
+                          $jcount++;
+                        //}
+                      }
+                      $kcount++;
+                      $jcount=0;
                       /*if ($j%48==0) {
                         $time[$jcount]=$j;
                         echo "48倍".$time[$jcount];
@@ -76,7 +88,10 @@
                         $jcount++;
                       }*/
                     }
-                    print_r($time);
+
+                    //print_r($time);
+                    //echo "string<br>";
+                    //var_dump($time);
                     echo '
                     <label for="time">店舗に予約可能な回数を入力し、設定を保存します。</label><br>
                     <form method="post" action="../shop" accept-charset="UTF-8">
@@ -100,31 +115,25 @@
                     }
                     $len = count($quantityArr2value);
                     $a = $len/7;
-                    for ($i=0; $i < $a ; $i++) {
+                    $count=0;
+                    //echo "time=".$timeArr4[$time[$count]].'value='.$quantityArr2value[$time[$count]];
+                    for ($i=0; $i < $len ; $i++) {
                       if ($i%7==0) {//７件づつ改行
                         if ($i!=0) {
                           echo '<tr></tr>';
                         }
                       }
                       if ($i%7==0) {//７件づつ時刻表示
-                        echo '<td align="right">'.$hour.":".$min.'</td>';
+                        echo '<td align="right">'.$hour.":".$min.'</td>';//テーブル左端の時刻作成
                         if ($min==0) {
                             $min=30;
                         }else {
                             $min=0;
                             $hour++;
                         }
-                        /*
-                        echo '<td>'.$hour.":".$min.'</td>';//テーブルヘッダーの時刻を作成・表示
-                        if ($min==0) {
-                            $min=30;
-                        }else {
-                            $min=0;
-                            $hour++;
-                        }
-                        */
                       }
-                      echo '<td><input type="number" name='.$timeArr4[$i].' value='.$quantityArr2value[$i].' class="sample4"></td>';
+                      echo '<td><input type="number" name='.$timeArr4[$time[$count]].' value='.$quantityArr2value[$time[$count]].' class="sample4"></td>';
+                      $count++;
                     }
                     echo "</table>";
                     echo '
@@ -133,181 +142,6 @@
                     </form>';
                 }
                 ?>
-<!--
-                    <table border="1">
-
-                      <tr>
-                        <td>-</td><td>月</td><td>火</td><td>水</td><td>木</td><td>金</td><td>土</td><td>日</td>
-                      </tr>
-                      <tr>
-                        <td>00:00</td>
-                        <td><input type="number" name="mon_00_00" width="10"></td>
-                        <td><input type="number" name="tue_00_00" width="10"></td>
-                        <td><input type="number" name="wed_00_00" width="10"></td>
-                        <td><input type="number" name="thu_00_00" width="10"></td>
-                        <td><input type="number" name="fri_00_00" width="10"></td>
-                        <td><input type="number" name="sat_00_00" width="10"></td>
-                        <td><input type="number" name="sun_00_00" width="10"></td>
-                      </tr>
-                      <tr>
-                        <td>00:30</td>
-                        <td><input type="number" name="mon_00_30" width="10"></td>
-                        <td><input type="number" name="tue_00_30" width="10"></td>
-                        <td><input type="number" name="wed_00_30" width="10"></td>
-                        <td><input type="number" name="thu_00_30" width="10"></td>
-                        <td><input type="number" name="fri_00_30" width="10"></td>
-                        <td><input type="number" name="sat_00_30" width="10"></td>
-                        <td><input type="number" name="sun_00_30" width="10"></td>
-                      </tr>
-                      <tr>
-                        <td>01:00</td>
-                        <td><input type="number" name="mon_01_00" width="10"></td>
-                        <td><input type="number" name="tue_01_00" width="10"></td>
-                        <td><input type="number" name="wed_01_00" width="10"></td>
-                        <td><input type="number" name="thu_01_00" width="10"></td>
-                        <td><input type="number" name="fri_01_00" width="10"></td>
-                        <td><input type="number" name="sat_01_00" width="10"></td>
-                        <td><input type="number" name="sun_01_00" width="10"></td>
-                      </tr>
-                      <tr>
-                        <td>01:30</td>
-                      </tr>
-                      <tr>
-                        <td>02:00</td>
-                      </tr>
-                      <tr>
-                        <td>02:30</td>
-                      </tr>
-                      <tr>
-                        <td>03:00</td>
-                      </tr>
-                      <tr>
-                        <td>03:30</td>
-                      </tr>
-                      <tr>
-                        <td>04:00</td>
-                      </tr>
-                      <tr>
-                        <td>04:30</td>
-                      </tr>
-                      <tr>
-                        <td>05:00</td>
-                      </tr>
-                      <tr>
-                        <td>05:30</td>
-                      </tr>
-                      <tr>
-                        <td>06:00</td>
-                      </tr>
-                      <tr>
-                        <td>06:30</td>
-                      </tr>
-                      <tr>
-                        <td>07:00</td>
-                      </tr>
-                      <tr>
-                        <td>07:30</td>
-                      </tr>
-                      <tr>
-                        <td>08:00</td>
-                      </tr>
-                      <tr>
-                        <td>08:30</td>
-                      </tr>
-                      <tr>
-                        <td>09:00</td>
-                      </tr>
-                      <tr>
-                        <td>09:30</td>
-                      </tr>
-                      <tr>
-                        <td>10:00</td>
-                      </tr>
-                      <tr>
-                        <td>10:30</td>
-                      </tr>
-                      <tr>
-                        <td>11:00</td>
-                      </tr>
-                      <tr>
-                        <td>11:30</td>
-                      </tr>
-                      <tr>
-                        <td>12:00</td>
-                      </tr>
-                      <tr>
-                        <td>12:30</td>
-                      </tr>
-                      <tr>
-                        <td>13:00</td>
-                      </tr>
-                      <tr>
-                        <td>13:30</td>
-                      </tr>
-                      <tr>
-                        <td>14:00</td>
-                      </tr>
-                      <tr>
-                        <td>14:30</td>
-                      </tr>
-                      <tr>
-                        <td>15:00</td>
-                      </tr>
-                      <tr>
-                        <td>15:30</td>
-                      </tr>
-                      <tr>
-                        <td>16:00</td>
-                      </tr>
-                      <tr>
-                        <td>16:30</td>
-                      </tr>
-                      <tr>
-                        <td>17:00</td>
-                      </tr>
-                      <tr>
-                        <td>17:30</td>
-                      </tr>
-                      <tr>
-                        <td>18:00</td>
-                      </tr>
-                      <tr>
-                        <td>18:30</td>
-                      </tr>
-                      <tr>
-                        <td>19:00</td>
-                      </tr>
-                      <tr>
-                        <td>19:30</td>
-                      </tr>
-                      <tr>
-                        <td>20:00</td>
-                      </tr>
-                      <tr>
-                        <td>20:30</td>
-                      </tr>
-                      <tr>
-                        <td>21:00</td>
-                      </tr>
-                      <tr>
-                        <td>21:30</td>
-                      </tr>
-                      <tr>
-                        <td>22:00</td>
-                      </tr>
-                      <tr>
-                        <td>22:30</td>
-                      </tr>
-                      <tr>
-                        <td>23:00</td>
-                      </tr>
-                      <tr>
-                        <td>23:30</td>
-                      </tr>
-                      </tr>
-                    </table>
--->
-
               </br></br></br></br>
             </div>
         </div>
