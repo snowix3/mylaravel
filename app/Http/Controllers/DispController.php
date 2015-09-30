5589b2906ecce3b2da4feb6afc78f1c1469ea4c8
snowix3 <snowix3@gmail.com>
2015-09-30 20:26:00 +0900

    User予約画面ほぼ完成

--- app/Http/Controllers/DispController.php
+++ app/Http/Controllers/DispController.php
@@ -24,21 +24,77 @@ class DispController extends Controller
           $dbtime = $value2;
         }
       }
-      echo "index";
-      return view('reservation/disp')->with('dbtime',$dbtime);
+//      echo $dbtime;
+      //1.同じ日時の予約在庫数に問い合わせて設定を確認し、予約情報に問い合わせて差分がいくつあるかを表示する。
+      try {
+        $week = array("E004","mon", "tue", "wed", "thu", "fri", "sat", "sun");//曜日を表示するために文字を格納
+//        $search = $week[date('w',strtotime($dbtime))]."_".date('H_i', strtotime($dbtime));
+
+        $hour=0;
+        $min=0;
+        $c=0;
+        $dbcount=array();
+        /*日付・日時ごとの予約数を”横”に出力する*/
+        for ($i=0; $i < 48 ; $i++){
+          for ($j=0; $j < 7; $j++) {
+            $dbcountArr = DB::select('select count(reservation_time) from shop_reservation_jonathans
+            where reservation_time="'.date('Y-m-d ', strtotime($dbtime.("+$j day"))).$hour.':'.$min.':00"');
+            foreach ($dbcountArr as $key1 => $value1) {
+              foreach ($value1 as $key2 => $value2) {
+                $dbcount[$c] = $value2;
+              //  echo $dbcount[$c].",";
+                $c++;
+              }
+            }
+          }
+          if($min==0){//時刻を30分ずつずらす。
+              $min=30;
+          }else{//1時間増やす
+              $min=0;
+              $hour++;
+          }
+      //    echo "-<br>";
+        }
+        $hour=0;
+        $min=0;
+        $c=0;
+        /*日時ごとの予約在庫数を取りに行って”横”に出力していく。*/
+        $dbArr = DB::select('select * from shop_reservation_disp_jonathans where id=1');//ID=1のレコードを取りにいく。
+        $stock=array();
+        //曜日を数値で取得して数値から配列中の文字を選んで表示する。後半は$dbtimeの時_分という形式に整形。
+        for ($i=0; $i < 48 ; $i++) {
+          for ($j=0; $j < 7; $j++) {
+            $search = $week[date('N',strtotime($dbtime.("+$j day")))]."_".sprintf('%02d', $hour)."_".sprintf('%02d', $min);//"00_00"
+            foreach ($dbArr as $key1) {
+              $key=$key1;
+              $stock[$c] = ($key->$search);//キーを指定して値を取り出す。
+        //      echo $stock[$c];
+              $c++;
+            }
+          }
+      //    echo "-<br>";
+          if($min==0){//時刻を30分ずつずらす。
+              $min=30;
+          }else{//1時間増やす
+              $min=0;
+              $hour++;
+          }
+        }
+
+        /*計算 $stock(予約在庫数)-$dbcount(予約数)*/
+        $a = array();
+        for ($i=0; $i < 336 ; $i++) {
+          $a[$i] = $stock[$i]-$dbcount[$i];
+        //  echo $a[$i]."<br>";
+        }
+
+      } catch (Exception $e) {//エラー時
+//        echo "E003:".$e;
+        return view('reservation/disp')->with('e',$e);
+      }
+      return view('reservation/disp')->with('dbtime',$dbtime)->with('a',$a);
     }
-/*
-$now = DB::select('select current_timestamp');//DBの現在時刻を取りに行く
-$dbtime;//DBの現在時刻を格納。
-foreach ($now as $key1 => $value1) {
-  foreach ($value1 as $key2 => $value2) {
-    $dbtime = $value2;
-  }
-}
-echo "aaaaaaaaaaaaa";
-echo $dbtime;
-return view('reservation/disp');
-*/
+
     /**
      * Show the form for creating a new resource.
      *
@@ -70,17 +126,11 @@ return view('reservation/disp');
       }
       DB::table('shop_reservation_jonathans')
       ->insert([//'user_name' => $user_name,
-      //'detail' => $detail, 
+      //'detail' => $detail,
       'reservation_time' => $reservation_time]);
-
-      //print_r($now);
-      /*日付から曜日を取得する*/
-      //$datetime = date_create($reservation_time);
       $datetime = date_create($dbtime);//DBの時刻をいれる
-      //echo $reservation_time;
       $week = array("日", "月", "火", "水", "木", "金", "土");
-      $w = (int)date_format($datetime, 'w');
-      //echo $week[$w];
+      $w = (int)date_format($datetime, 'w');//echo $week[$w];
       return 'store Successfully done!';
     }
 
--- resources/views/reservation/disp.blade.php
+++ resources/views/reservation/disp.blade.php
@@ -57,28 +57,33 @@
             <div class="content">
                 <div class="title">DISP OK</div>
                 <?php
+                if (isset($e)){
+                  echo "<br>".$e;
+                }
                 //  echo "クライアント時刻".date("Y-m-d H:i:s");//現在
                 //  echo "<br>";
                   //$datetime = date("Y-m-d H:i:s");//クライアントの時刻が2015-09-28 14:07:02の形式で入ってる
 
-                  $week = array("日", "月", "火", "水", "木", "金", "土");//曜日を表示するために文字を格納
-                //  echo $week[date('w',strtotime($dbtime))]."曜日";
-                  //echo date("m月");
+                  $week = array("E005","月", "火", "水", "木", "金", "土", "日");//曜日を表示するために文字を格納
+                  //"mon"0, "tue"1, "wed"2, "thu"3, "fri"4, "sat"5, "sun"6 こっちにしたい
+                  //"sun"0,"mon"1, "tue"2, "wed"3, "thu"4, "fri"5, "sat"6 本当はこっち
+                  //echo $dbtime."<br>";
+                  //echo $week[date('N',strtotime($dbtime))]."<br>";
+                  //echo date('N',strtotime($dbtime))."<br>";
                 ?>
-
 
                   <table border="1" class="tdlink">
                     <th colspan="8">本日から6日後まで予約できます。</th>
                     <tr></tr>
                     <!--１行目に曜日を表示する。取得した$dbtimeを元に曜日を数値で出して、配列の文字列に置き換えて表示。-->
                     <th>---</th>
-                    <th><?php echo $week[date('w',strtotime($dbtime))]; ?></th>
-                    <th><?php echo $week[date('w',strtotime($dbtime.("+1 day")))]; ?></th>
-                    <th><?php echo $week[date('w',strtotime($dbtime.("+2 day")))]; ?></th>
-                    <th><?php echo $week[date('w',strtotime($dbtime.("+3 day")))]; ?></th>
-                    <th><?php echo $week[date('w',strtotime($dbtime.("+4 day")))]; ?></th>
-                    <th><?php echo $week[date('w',strtotime($dbtime.("+5 day")))]; ?></th>
-                    <th><?php echo $week[date('w',strtotime($dbtime.("+6 day")))]; ?></th>
+                    <th><?php echo $week[date('N',strtotime($dbtime))]; ?></th>
+                    <th><?php echo $week[date('N',strtotime($dbtime.("+1 day")))]; ?></th>
+                    <th><?php echo $week[date('N',strtotime($dbtime.("+2 day")))]; ?></th>
+                    <th><?php echo $week[date('N',strtotime($dbtime.("+3 day")))]; ?></th>
+                    <th><?php echo $week[date('N',strtotime($dbtime.("+4 day")))]; ?></th>
+                    <th><?php echo $week[date('N',strtotime($dbtime.("+5 day")))]; ?></th>
+                    <th><?php echo $week[date('N',strtotime($dbtime.("+6 day")))]; ?></th>
                     <tr></tr>
                     <!--２行目には$dbtimeを元に当日含め１週間、６日後までを表示する。-->
                     <td></td>
@@ -95,27 +100,35 @@
                     $hour=0;
                     $min=0;
                     $c=0;
+                    //1.同じ日時の予約在庫数に問い合わせて設定を確認し、予約情報に問い合わせて差分がいくつあるかを表示する。
+                    //2.同じ場所からaタグクリックで予約追加もする。
+                    //3.現在時刻以前と＋２時間後までを予約不可能にする。
                     for ($n=0; $n < 48 ; $n++) {
                       echo '<td align="right">'.sprintf('%02d', $hour).":".sprintf('%02d', $min).'</td>';//テーブル左端の時刻作成
                       for ($i=0; $i < 7 ; $i++) {
                         /*このaタグを押すと同じ$cの値のformが押される。各マスごとの時間が所定の形式でコントローラーに送られ、DBに入る。*/
-                        echo '<td  align="center"><a href="../disp" onclick="document.form'.$c.'.submit();return false;">O</a></td>';
-                        echo '
-                        <form name="form'.$c.'" method="post" action="../disp" accept-charset="UTF-8">
-                          <input type="hidden" name="reservation_time" value="'.date('Y-m-d ', strtotime($dbtime."+$i day"))."$hour:$min:00".'">
-                          <input type="hidden" name="_token" value="'.csrf_token().'">
-                        </form>
-                        ';
+                        //在庫数が０なら弾く'.$a[$c].'
+                        if ($a[$c]>0) {
+                          echo '<td  align="center"><a href="../disp" onclick="document.form'.$c.'.submit();return false;">O</a></td>';
+                          echo '
+                          <form name="form'.$c.'" method="post" action="../disp" accept-charset="UTF-8">
+                            <input type="hidden" name="reservation_time" value="'.date('Y-m-d ', strtotime($dbtime."+$i day"))."$hour:$min:00".'">
+                            <input type="hidden" name="_token" value="'.csrf_token().'">
+                          </form>
+                          ';
+                        }else {
+                          echo '<td  align="center"><a href="#" onclick="document.form'.$c.'.submit();return false;">X</a></td>';
+                        }
                         $c++;
                       }
-                        echo "<tr></tr>";
-                        if($min==0){
-                            $min=30;
-                        }else{
-                            $min=0;
-                            $hour++;
-                        }
+                      echo "<tr></tr>";
+                      if($min==0){//時刻を３０分ずつずらす。
+                          $min=30;
+                      }else{//1時間増やす
+                          $min=0;
+                          $hour++;
                       }
+                    }
                     ?>
                     <?php
 //                    echo '<td name='.date('Y-m-d ', strtotime($dbtime))."00:00:00".'value='.date('Y-m-d ', strtotime($dbtime))."00:00:00".'>
